ef299141ccbafc5fb7aaedf154e5f7daa3f90c04
hnitek <hnitek@gmail.com>
2022-10-23 13:31:55 +0200

    Add initial components, models and service

--- recipes-book-front/src/app/app-routing.module.ts
+++ recipes-book-front/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 'path': '', component: HomeComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- recipes-book-front/src/app/app.module.ts
+++ recipes-book-front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
